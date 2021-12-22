774605b6bba57961605e0cab6f20bed1124b8564
Matias Camiletti <matias.camiletti@gmail.com>
2021-12-21 23:20:36 -0300

    Copy library.

--- projects/agencycoda/mia-quill-image-resize/src/lib/mia-quill-image-resize.module.ts
+++ projects/agencycoda/mia-quill-image-resize/src/lib/mia-quill-image-resize.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
-import { MiaQuillImageResizeComponent } from './mia-quill-image-resize.component';
-
 
 
 @NgModule({
   declarations: [
-    MiaQuillImageResizeComponent
+    
   ],
   imports: [
   ],
   exports: [
-    MiaQuillImageResizeComponent
+    
   ]
 })
 export class MiaQuillImageResizeModule { }
--- projects/agencycoda/mia-quill-image-resize/src/public-api.ts
+++ projects/agencycoda/mia-quill-image-resize/src/public-api.ts
@@ -2,6 +2,11 @@
  * Public API Surface of mia-quill-image-resize
  */
 
-export * from './lib/mia-quill-image-resize.service';
-export * from './lib/mia-quill-image-resize.component';
+export * from './lib/modules/BaseModule';
+export * from './lib/modules/DisplaySize';
+export * from './lib/modules/Resize';
+export * from './lib/modules/Toolbar';
+export * from './lib/DefaultOptions';
+export * from './lib/ImageResize';
+
 export * from './lib/mia-quill-image-resize.module';
